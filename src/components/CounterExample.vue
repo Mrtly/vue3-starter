db8801f9a9816689e233c81ce592a2e2a6551dad
Myrtle <mrtly.ca@gmail.com>
2021-06-04 17:14:54 -0700

    Update package

--- src/components/CounterExample.vue
+++ src/components/CounterExample.vue
@@ -1,29 +1,36 @@
 <template>
-  <div>{{ msg }}</div>
-	<button @click='increment' class="mt-4 uppercase px-8 py-2 rounded-full bg-green-300 text-green-600 max-w-max shadow-sm hover:shadow-md">click</button>
-  <div class="mt-4 m-auto text-lg text-green-600 max-w-max shadow-sm ">count is: {{ count }}</div>
+  <div class="text-xl mb-5">{{ msg }}</div>
+  <button
+    @click="increment"
+    class="mt-4 uppercase font-bold tracking-widest px-8 py-2 rounded-full bg-green-600 text-green-200 max-w-max shadow-sm hover:shadow-md focus:outline-none"
+  >
+    click
+  </button>
+  <div class="mt-4 m-auto text-xl font-semibold tracking-wider text-green-600">
+    count is: {{ count }}
+  </div>
+  <button
+    @click="reset"
+    class="mt-8 px-4 py-1 rounded-full bg-green-200 text-green-600 max-w-max shadow-sm hover:shadow-md focus:outline-none"
+  >
+    reset
+  </button>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from "vue";
+<script setup lang="ts">
+import { ref, defineProps } from "vue";
 
-export default defineComponent({
-  props: {
-    msg: {
-      type: String,
-      default: 'vite starter'
-    }
-  },
-  setup() {
-    let count: number = ref('0');
-
-    function increment(){
-      count.value++
-    }
-
-    return {
-      count, increment
-    };
-  },
+defineProps({
+  msg: { type: String, default: "vite starter" },
 });
+
+let count: number = ref("0");
+
+function increment() {
+  count.value++;
+}
+
+function reset() {
+  count.value = 0;
+}
 </script>
